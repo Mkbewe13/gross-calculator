965580ef4430b2fbe2b86bc09b9b9a06a7184291
Mkbewe13 <jakub@j15.digital>
2022-12-25 16:34:35 +0100

    INIT

---
+++ gross-calculator-plugin.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Name:     Gross Calculator
+ * Description:     Adds custom shortcode with a form where you can create a post with gross and tax value calculation. The newly added post type will have a custom post type: gross-calculation
+ * Author:          Jakub Owczarek
+ * Text Domain:     GC
+ * Version:         0.1
+ */
+
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('APP_DIR', dirname(__FILE__));
+
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+class GrossCalculator{
+
+    public function init(){
+
+        add_action('init', [$this,'registerCPTs'], 0);
+
+        add_action('init',[$this,'registerShortCodes'], 0);
+
+    }
+
+    public function registerCPTs(){
+        //register cpt class
+    }
+
+    public function registerShortCodes(){
+       //register shortcode class
+    }
+
+
+}
+
+$grossCalculator = new GrossCalculator();
+$grossCalculator->init();
---
+++ readme.md
@@ -0,0 +1,5 @@
+A Wordpress plugin that allows you to add a shordcode with a form that calculates the gross value and tax. The score is calculated based on the product data provided.
+
+Setup: composer install
+
+
